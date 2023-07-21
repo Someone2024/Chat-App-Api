07ffb594390f9f3e115007c9ba70727fe5d1c98e
santiago <bassilsantiago@gmail.com>
2023-07-21 07:10:17 -0500

    Function to check auth state

---
+++ controllers/CheckAuth.js
@@ -0,0 +1,29 @@
+// middleware.js
+const jwt = require('./jwt');
+
+// Middleware function to check if the token is valid
+const checkAuth = (req, res, next) => {
+  const token = req.headers.authorization; // Assuming you're sending the token in the "Authorization" header
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized. No token provided.' });
+  }
+
+  try {
+    // Verify the token
+    const decodedToken = jwt.verifyToken(token);
+
+    // Attach the decoded token to the request object for use in other routes
+    req.userId = decodedToken.userId;
+
+    // Move to the next middleware/route handler
+    next();
+  } catch (error) {
+    console.error('Error while verifying token:', error);
+    res.status(401).json({ message: 'Unauthorized. Invalid token.' });
+  }
+};
+
+module.exports = {
+  checkAuth,
+};
