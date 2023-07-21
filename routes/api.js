8894f5edb6987ac3a822e5452f5f1e2f9871f268
santiago <bassilsantiago@gmail.com>
2023-07-21 07:09:41 -0500

    Feat: Added authentication controllers

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const router = express.Router();
+const loginController = require("../controllers/loginController")
+const messagesController = require("../controllers/messagesController")
+const checkAuth = require("../controllers/CheckAuth")
 
-router("/login")
+router("/login", loginController.logIn)
 
-router("/messages")
+router("/messages", checkAuth.checkAuth, messagesController.displayMessages)
 
-router("/messages/send-message")
+router("/messages/send-message", checkAuth.checkAuth, messagesController.sendMessage)
+
+module.exports = router;
